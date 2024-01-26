1b624e86d26f9a833c90cb9deef8396786b0f061
mg_dell <bevilledavid@gmail.com>
2024-01-26 13:48:50 +0100

    26/01/2024 add compileOptions and kotlinOptions

--- lib/src/google_map_location_picker.dart
+++ lib/src/google_map_location_picker.dart
@@ -145,7 +145,7 @@ class LocationPickerState extends State<LocationPicker> {
       ),
     );
 
-    Overlay.of(context)!.insert(overlayEntry!);
+    Overlay.of(context).insert(overlayEntry!);
 
     autoCompleteSearch(place);
   }
@@ -257,7 +257,7 @@ class LocationPickerState extends State<LocationPicker> {
       ),
     );
 
-    Overlay.of(context)!.insert(overlayEntry!);
+    Overlay.of(context).insert(overlayEntry!);
   }
 
   /// Utility function to get clean readable name of a location. First checks
